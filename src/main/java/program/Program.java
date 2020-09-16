c234b6419b43a4ede975f0027d8df95b4c883ec9
artemGBF <shipunov.temak@yandex.ru>
2020-09-16 20:29:35 +0300

    DEV-4 changes

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -9,6 +9,6 @@ public class Program {
         System.out.println("Hello!");
         System.out.println("My name is Sam Jonson");
         System.out.println("Merge fail");
-        System.out.println("12334");
+        System.out.println("000000");
     }
 }
