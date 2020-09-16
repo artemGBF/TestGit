68c17c96df180f07e49073d93f3585bc91705ff5
aitunin <2323353@gmail.com>
2020-09-16 20:03:43 +0300

    DEV-2 знакомство

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -7,5 +7,6 @@ package program;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("My name is Ivan Ivanov");
     }
 }
