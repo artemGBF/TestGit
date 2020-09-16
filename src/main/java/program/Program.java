0d4412495854fc374de2e7c43747e4d5915db58a
artemGBF <50134582+artemGBF@users.noreply.github.com>
2020-09-16 20:19:50 +0300

    DEV-3 add number (#3)

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -9,5 +9,6 @@ public class Program {
         System.out.println("Hello!");
         System.out.println("My name is Sam Jonson");
         System.out.println("Merge fail");
+        System.out.println("12334");
     }
 }
